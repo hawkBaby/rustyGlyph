b21de0f33a478c8c08f51237392eb9d1afca7b4b
Jad Pamment <jad.pamment@assurity.co.nz>
2018-10-14 18:28:35 +1300

    Updated loop

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
 extern crate image;
 extern crate nalgebra as na;
 
-use image::JPEG;
-use image::PNG;
+//use image::JPEG;
+//use image::PNG;
 use image::Rgb;
-use image::{GenericImage,GenericImageView,Pixel};
+//use image::{GenericImage,GenericImageView,Pixel};
 use std::thread;
 use std::sync::{Mutex, Arc};
-use std::time::Duration;
+//use std::time::Duration;
 use na::base::Matrix3;
 use na::base::Matrix3x1;
 
 fn opt_anaglyph_gen(li_p:&image::Rgb<u8>, ri_p:&image::Rgb<u8>) -> Vec<u8> {
-    //println!("Building pixel value");
     let _l_vec = li_p.clone();
     let _r_vec = ri_p.clone();
     let mut out_vec = Vec::new();
@@ -39,30 +38,29 @@ fn main() {
     let _l_img = image::open("./inputImages/imageLeft.jpg").unwrap().to_rgb();
     let _r_img = image::open("./inputImages/imageRight.jpg").unwrap().to_rgb();
     let mut handles = vec![];
-    // The dimensions method returns the images width and height.
-    //let top_sec = (_r_img.height() as f32 * 0.25) as u32;
     let width = _l_img.width();
     let height = _l_img.height();
     
-    let mut out_img = image::ImageBuffer::<Rgb<u8>, Vec<u8>>::new(width, height);
+    let out_img = image::ImageBuffer::<Rgb<u8>, Vec<u8>>::new(width, height);
 
     let _mod_img = Arc::new(Mutex::new(out_img));
     let _l_img_mut = Arc::new(Mutex::new(_l_img));
     let _r_img_mut = Arc::new(Mutex::new(_r_img));
-    let loop_counter = 11;
+    let loop_counter = 20;
     let _h_ = height.clone();
 
-    for el in 1..loop_counter {
-        
+    for el in 0..loop_counter {
         let _mod_img_a = Arc::clone(&_mod_img);
         let _l_img_a = Arc::clone(&_l_img_mut);
         let _r_img_a = Arc::clone(&_r_img_mut);
-        let t_l = (el as f32/loop_counter as f32);
+        let t_m = el as f32/loop_counter as f32;
+        let t_l = (el + 1) as f32/loop_counter as f32;
+        let _height_m = (_h_ as f32 * t_m) as u32;
         let _height_a = (_h_ as f32 * t_l) as u32;
-        println!("Height: {:?}",_height_a);
+        println!("Start: {:?} || End: {:?} || Total: {:?} || Current: {:?} || End Count: {:?}",_height_m, _height_a, _h_,el,loop_counter);
         let handle = thread::spawn(move || {
             for _i in 0..width {
-                for _it in 0.._height_a {
+                for _it in _height_m.._height_a {
                     let mut _img  = _mod_img_a.lock().unwrap();
                     let _l_o = _l_img_a.lock().unwrap();
                     let _r_o = _r_img_a.lock().unwrap();
@@ -82,5 +80,5 @@ fn main() {
     }
 
    let out_val = _mod_img.lock().unwrap();
-   out_val.save("outputImage/output.png").unwrap();
+   out_val.save("outputImage/output.jpg").unwrap();
 }
